e9e7702365a70c46971fda579c57218e8cf8a283
HarrisonNo <72544956+HarrisonNo@users.noreply.github.com>
2023-12-18 00:12:02 -0800

    Completed compilation prep work, now can easily compile

--- bob_the_builder.py
+++ bob_the_builder.py
@@ -5,13 +5,13 @@ import shutil
 #This sucked literal ass since walk() genuinely doesn't work properly with subdirectories within subdirectories
 def copy_file(file):
     if file.endswith('.h') or file.endswith('.cpp'):
-        print("COPYING FILE:"+file)
+        #print("COPYING FILE:"+file)
         shutil.copy2(file,'./construction_yard')  
 
 def recursive_search_dir(path):
     listing = os.listdir(path)
     for item in listing:
-        print("THE CURRENT ITEM IS:"+item)
+        #print("THE CURRENT ITEM IS:"+item)
         if os.path.isfile(item) or item.find('.') != -1:
             copy_file(os.path.join(path, item))
         else:
@@ -19,20 +19,26 @@ def recursive_search_dir(path):
                 new_path = os.path.join(path, item)
                 recursive_search_dir(new_path)
 
+def delete_construction_yard_sources():
+    listing = os.listdir(".")
+    for item in listing:
+        #print("THE CURRENT ITEM IS:"+item)
+        if os.path.isfile(item) and (item.endswith('.h') or item.endswith('.cpp')):
+            #print("ATTEMPTING TO DELETE ITEM: "+item)
+            os.remove(item)
 
 
 #MAIN
+#Check if our number of args is valid
 if (len(sys.argv) != 2):
     print("Please enter exactly one argument with this script: <all> or <clean> are valid")
     quit()
-
-#TODO Implement deleting previous files as I have no idea if copy2 overwrites files (not that I even trust the python implementation enough to rely on it)
-
+#Copy all source files to the construction_yard
 recursive_search_dir(".")
-
-
-
-#os.chdir("construction_yard")
-#make_string = "make " + sys.argv[1]
-#print("Calling make as:" + make_string)
-#os.system(make_string)
+#Run the makefile present in the construction yard with the requested arg
+os.chdir("construction_yard")
+make_string = "make " + sys.argv[1]
+print("Calling makefile with: " + make_string)
+os.system(make_string)
+#Delete all source files in the construction_yard
+delete_construction_yard_sources()
--- construction_yard/global_defines.h
+++
@@ -1,4 +0,0 @@
-#define VERIFY_CRASH 1
-#define ASSERT_CRASH 1
-#define VERBOSE 1
-#define DEBUG_API 1
